8d831277c9a1d128c5456976564091e6b11617a3
HimaniTawade <10599696-HimaniTawade@users.noreply.replit.com>
2023-04-07 23:28:07 +0000

    First Api Jobs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -34,6 +34,9 @@ JOBS = [
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='TCS')
 
-print(__name__)
+@app.route("/api/jobs")
+def list_jos():
+  return jsonify(JOBS)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug= True)
