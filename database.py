c56bba9a299d3f3846f1c25c3105444000d1e04f
HimaniTawade <10599696-HimaniTawade@users.noreply.replit.com>
2023-04-08 04:18:15 +0000

    config

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://3ijqub94u1owp8bn7d4b:pscale_pw_If9V6T1dNhNPrx9eedPbBtsRqt2isOzaq8xxHPUjod4@aws.connect.psdb.cloud/vegitizerwebapp?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://gxc6jbmuarolf7577bdt:pscale_pw_55uMvagh0XxezA7T0Q7U9rzt8secCvVinAb7CXliAWO@ws.connect.psdb.cloud/vegitizerwebapp?charset=utf8mb4"
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
