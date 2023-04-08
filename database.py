59b24ca1691567c1305eae9c64b9c6d95da4d161
HimaniTawade <10599696-HimaniTawade@users.noreply.replit.com>
2023-04-08 04:39:38 +0000

    Secret

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = "mysql+pymysql://3pp3x53q6e904br93px7:pscale_pw_kjC25WE4MayogwMbyF8e7IZPNU8vxaSk9bsMlHlMFo5@aws.connect.psdb.cloud/vegitizerwebapp?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
