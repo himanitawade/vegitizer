9ffdb2a769fdd20842a8be14262560a9df4042ff
HimaniTawade <10599696-HimaniTawade@users.noreply.replit.com>
2023-04-09 00:48:03 +0000

    Validations

--- database.py
+++ database.py
@@ -29,3 +29,21 @@ def load_job_from_db(id):
     else:
       value = rows[0]._asdict()
       return value
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    val = {
+      'job_id': job_id,
+      'full_name': data['full_name'],
+      'email': data['email'],
+      'linkedin_url': data['linkedin_url'],
+      'education': data['education'],
+      'work_experience': data['work_experience'],
+      'resume_url': data['resume_url']
+    }
+    query = text(
+      "INSERT INTO applications(job_id,full_name,email,linkedin_url,education,work_experience,resume_url) VALUES (:job_id,:full_name,:email,:linkedin_url,:education,:work_experience,:resume_url)"
+    )
+
+    conn.execute(query, val)
